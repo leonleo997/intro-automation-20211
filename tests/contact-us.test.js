ead5f8b00f2152f691d1b26fdb20c0266ed6fe30
Yesid Lopez <yesid.lopezs@perficient.com>
2021-11-05 21:20:52 -0500

    add contact test scheme

---
+++ tests/contact-us.test.js
@@ -0,0 +1,16 @@
+import {Selector} from 'testcafe'
+
+fixture("Contact us feature").page('http://automationpractice.com/')
+
+test.only('The contact us option is successfully', async (t) => {
+    // Arrange
+    const subjectHeading = ""
+    const emailAdress = ""
+    const message = ""
+
+    // Act
+    
+
+    // Assert
+    
+});
