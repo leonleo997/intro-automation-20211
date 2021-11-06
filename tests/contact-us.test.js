10f07630b0820fbb6771590b5a67753dc7c5912b
Yesid Lopez <yesid.lopezs@perficient.com>
2021-11-05 21:44:49 -0500

    add contact us body

--- tests/contact-us.test.js
+++ tests/contact-us.test.js
@@ -2,15 +2,21 @@ import {Selector} from 'testcafe'
 
 fixture("Contact us feature").page('http://automationpractice.com/')
 
-test.only('The contact us option is successfully', async (t) => {
+test('The contact us option is successfully', async (t) => {
     // Arrange
-    const subjectHeading = ""
-    const emailAdress = ""
-    const message = ""
+    const subjectHeading = "Webmaster"
+    const emailAdress = "john.doe@test.com"
+    const message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor commodo ex a ultrices. Suspendisse quis vulputate ante. Aenean maximus."
 
     // Act
-    
+    await t.click("")
+    await t.click("")
+    await t.click(Selector("").withText(subjectHeading))
+    await t.typeText("", emailAdress)
+    await t.typeText("", message)
+    await t.click("")
 
     // Assert
-    
+    await t.expect(Selector("#center_column p").textContent).eql("Your message has been successfully sent to our team.")
+
 });
